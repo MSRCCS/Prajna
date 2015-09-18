47403dc5ddfdd7e0f5464fbaea3499689315f9f8
zhuweirong <weirong.zhu@outlook.com>
2015-09-18 16:26:10 -0700

    Bump version to 0.0.1.3-alpha

--- src/ServiceLib/ServiceEndpoint/AssemblyInfo.fs
+++ src/ServiceLib/ServiceEndpoint/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Prajna.ServiceEndpoint")>]
 [<assembly: AssemblyProductAttribute("Prajna")>]
 [<assembly: AssemblyDescriptionAttribute("Prajna: A Distributed Functional Programming Platform for Interactive Big Data Analytics and Cloud Service Building")>]
-[<assembly: AssemblyVersionAttribute("0.0.1.2")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.1.2")>]
+[<assembly: AssemblyVersionAttribute("0.0.1.3")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1.3")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.1.2"
+    let [<Literal>] Version = "0.0.1.3"

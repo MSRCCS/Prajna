68a9fa5201933b15305915aa03d7218d91997f09
BrunoBozza <brunosb@microsoft.com>
2015-10-02 17:06:21 -0700

    Adding Matrix-Matrix sample, updating Matrix-Vector

--- samples/examples/FSharpExamples/Matrix.fs
+++ samples/examples/FSharpExamples/Matrix.fs
@@ -28,38 +28,53 @@ open Prajna.Api.FSharp
 
 open Prajna.Examples.Common
 
-type MatrixSample() =
+module Matrix =
 
     let (.*) (xs: float[]) (ys: float[]) = 
-        // simulate doing some more work, 
-        // e.g.: run the remaining layers of a neural net
-        Threading.Thread.Sleep(3) 
         Array.fold2 (fun sum x y -> sum + x * y) 0.0 xs ys 
 
     let printTiming (msg: string) (x: Lazy<'b>) : 'b =
         let sw = Stopwatch.StartNew()
+        printf "%s: " msg
         let ret = x.Value
         sw.Stop()
-        printfn "%s: %A" msg sw.Elapsed
+        printfn "%A" (float sw.ElapsedMilliseconds / 1000.0)
         ret
 
-    let run (cluster: Cluster) =
+    let random = new Random(0)
+
+    let randomVector n =  Array.init n (fun _ -> random.NextDouble())
+    
+    let randomMatrix m n =  Array.init m (fun _ -> randomVector n)
+
+    /// Matrix-Vector multiplication for illustrational purposes.
+    /// The overhead of distributing makes it faster to run this locally.
+    /// See Matrix-Matrix for a case where the computation is actually faster distributed.
+    let matrixVector (cluster: Cluster) =
 
         let numRows = 200
         let vectorSize = 100
 
-        let r = new Random(0)
-        let randomMatrix =  Array.init numRows (fun _ -> Array.init vectorSize (fun _ -> r.NextDouble()))
-        let vector = Array.init vectorSize (fun _ -> r.NextDouble())
+        let matrix =  randomMatrix numRows vectorSize
+        let vector = randomVector vectorSize
 
         let singleCoreResult = 
             printTiming "single core" <| lazy 
-                randomMatrix |> Array.map (fun row -> row .* vector)
-        
+                matrix |> Array.map (fun row -> row .* vector)
+
+        let numPartitions = Environment.ProcessorCount
+        let dSet =
+            printTiming "distributing" <| lazy
+                let d =
+                    DSet<float[]>(Cluster = cluster, NumParallelExecution = numPartitions)
+                    |> DSet.distributeN numPartitions matrix
+                // Force push DSet to cluster
+                do d |> DSet.fold (fun _ _ -> 0) (fun _ _ -> 0) 0 |> ignore
+                d
+
         let distributedResult = 
-            printTiming "distributed" <| lazy 
-                DSet<float[]>(Cluster = cluster)
-                |> DSet.distribute randomMatrix
+            printTiming "computing distributed" <| lazy 
+                dSet
                 |> DSet.mapi(fun i j row -> (i,j), row .* vector)
                 |> DSet.toSeq
                 |> Seq.sortBy fst
@@ -68,8 +83,62 @@ type MatrixSample() =
 
         singleCoreResult = distributedResult
 
+    /// For simplicity, we implement our own matrix multiply out of simple dot products.
+    /// Matrix-Matrix actually makes sense to do distributed, even with our simple implementation.
+    /// For best performance, use a DSet of sub-matrices (float[,]), split rowwise, one sub-matrix
+    /// per node, and a linear algebra library such as BLAS for the local computation.
+    let matrixMatrix(cluster: Cluster) =
+
+        // To see the speedup of parallelism, set this to something higher, like 1500
+        let S = 120
+
+        // We will multiply matrix A(M x N) by B(N x P) 
+        // A is stored as a jagged array, snd will be split row-wise among nodes
+        // B is stored as a *transposed* jagged array, and will be replicated to every node
+        let M = S
+        let N = S
+        let P = S
+
+        let A = randomMatrix M N 
+        let B_transpose = randomMatrix P N
+
+        let singleCoreResult = 
+            printTiming "single core" <| lazy 
+                A |> Array.map (fun row -> Array.init P (fun j -> row .* B_transpose.[j]))
+
+        let numPartitions = cluster.NumNodes * 50
+
+        let dSet =
+            printTiming "distributing" <| lazy
+                let d =
+                    DSet<float[]>(Cluster = cluster, NumParallelExecution = numPartitions) 
+                    |> DSet.distributeN numPartitions A
+                    |> DSet.cacheInMemory
+                // Force push DSet to cluster
+                do d |> DSet.fold (fun _ _ -> 0) (fun _ _ -> 0) 0 |> ignore
+                d 
+
+        let distributedResult = 
+            printTiming "computing distributed" <| lazy 
+                dSet
+                |> DSet.mapi(fun i j row -> (i,j), Array.init P (fun j -> row .* B_transpose.[j]))
+                |> DSet.toSeq
+                |> Seq.sortBy fst
+                |> Seq.map snd
+                |> Seq.toArray
+  
+        singleCoreResult = distributedResult
+
+open Matrix
+
+type MatrixVector() =
     interface IExample with
-        member x.Description: string = "Distributed matrix vector multiply, simulating some extra work"
-        member x.Run(cluster: Cluster): bool = run cluster
+        member x.Description: string = "Distributed matrix-vector multiply"
+        member x.Run(cluster: Cluster): bool = matrixVector cluster
+        
+type MatrixMatrix() =
+    interface IExample with
+        member x.Description: string = "Distributed matrix-matrix vector multiply"
+        member x.Run(cluster: Cluster): bool = matrixMatrix cluster
         
 

2acb521327be9c7042334860dce5a9282acd083b
BrunoBozza <brunosb@microsoft.com>
2016-03-02 11:32:15 -0800

    Fixing build and removing auto-deploy to OneNet cluster

--- src/Nano/NanoServer/AssemblyInfo.fs
+++ src/Nano/NanoServer/AssemblyInfo.fs
@@ -1,41 +1,12 @@
-﻿namespace NanoServer.AssemblyInfo
-
+namespace System
 open System.Reflection
-open System.Runtime.CompilerServices
-open System.Runtime.InteropServices
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[<assembly: AssemblyTitle("NanoServer")>]
-[<assembly: AssemblyDescription("Prajna NanoServer: A minimalist server for Prajna")>]
-[<assembly: AssemblyConfiguration("")>]
-[<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("Prajna")>]
-[<assembly: AssemblyCopyright("Copyright ©  2016")>]
-[<assembly: AssemblyTrademark("")>]
-[<assembly: AssemblyCulture("")>]
+[<assembly: AssemblyTitleAttribute("Prajna.NanoServer")>]
+[<assembly: AssemblyProductAttribute("Prajna")>]
+[<assembly: AssemblyDescriptionAttribute("Prajna: A Distributed Functional Programming Platform for Interactive Big Data Analytics and Cloud Service Building")>]
+[<assembly: AssemblyVersionAttribute("0.0.1.7")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.1.7")>]
+do ()
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[<assembly: ComVisible(false)>]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[<assembly: Guid("d5cab179-54a9-44e5-acc9-d1c954a87f5b")>]
-
-// Version information for an assembly consists of the following four values:
-// 
-//       Major Version
-//       Minor Version 
-//       Build Number
-//       Revision
-// 
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
-
-do
-    ()
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.0.1.7"
